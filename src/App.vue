38fee77d3472de3be0adad36f48a2472415fa11d
giorgip62 <giorgiop62@gmail.com>
2022-11-17 16:32:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+  name:'App',
+
+}
+</script>
+
+<template>
+  <div class="container">
+    bootsrap
+  </div>
+  
+</template>
+
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
